7323c825a0664564b29b0e49b6b027bac64fe81a
Cristian Müller <mueller@berlus.de>
2016-10-12 16:02:37 +0200

    fixed move-in-date sanity check

--- app/Http/Requests/Modules/Mietvertraege/StoreMietvertraegeRequest.php
+++ app/Http/Requests/Modules/Mietvertraege/StoreMietvertraegeRequest.php
@@ -14,12 +14,15 @@ class StoreMietvertraegeRequest extends MietvertraegeRequest
         $move_in_date_rule = 'required|date';
         $v = Validator::make($this->all(), ['unit' => 'required|integer']);
         if ($v->valid()) {
-            $units = DB::select("SELECT EINHEIT.EINHEIT_ID, TYP, EINHEIT_KURZNAME, IF(MIN(MIETVERTRAG_BIS) = '0000-00-00', MIN(MIETVERTRAG_BIS), MAX(MIETVERTRAG_BIS)) AS MIETVERTRAG_BIS 
-                                 FROM MIETVERTRAG RIGHT JOIN EINHEIT ON (EINHEIT.EINHEIT_ID = MIETVERTRAG.EINHEIT_ID) 
-                                 WHERE MIETVERTRAG_AKTUELL = '1' AND EINHEIT_AKTUELL = '1' 
-                                  AND EINHEIT.EINHEIT_ID = ? GROUP BY EINHEIT.EINHEIT_ID 
-                                 HAVING MIETVERTRAG_BIS != '0000-00-00'", [$this->get('unit')]);
-            if (is_array($units) && !empty($units)) {
+            $units = DB::select(
+                "SELECT EINHEIT.EINHEIT_ID, TYP, EINHEIT_KURZNAME, 
+                  IF(MIN(MIETVERTRAG_BIS) = '0000-00-00', MIN(MIETVERTRAG_BIS), MAX(MIETVERTRAG_BIS)) AS MIETVERTRAG_BIS 
+                FROM MIETVERTRAG RIGHT JOIN EINHEIT ON (EINHEIT.EINHEIT_ID = MIETVERTRAG.EINHEIT_ID) 
+                WHERE MIETVERTRAG_AKTUELL = '1' AND EINHEIT_AKTUELL = '1' 
+                  AND EINHEIT.EINHEIT_ID = ? GROUP BY EINHEIT.EINHEIT_ID 
+                HAVING MIETVERTRAG_BIS != '0000-00-00'", [$this->get('unit')]
+            );
+            if (!empty($units)) {
                 $move_in_date_rule .= '|after:' . $units[0]['MIETVERTRAG_BIS'];
             }
         }
