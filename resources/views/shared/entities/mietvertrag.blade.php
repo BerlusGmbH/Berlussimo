65f5ff8799695e10ab81c4e395a4efddc965be84
Cristian Müller <mueller@berlus.de>
2017-02-27 14:48:44 +0100

    added Mietkontenblatt to Mietvertrag entity identifier

--- resources/views/shared/entities/mietvertrag.blade.php
+++ resources/views/shared/entities/mietvertrag.blade.php
@@ -6,5 +6,6 @@
         <i class="mdi mdi-checkbox-blank-circle-outline tooltipped" data-position="bottom" data-delay="50"
            data-tooltip="Mietvertrag (Inaktiv)"></i>
     @endif
-    <a href="{{ route('web::uebersicht::legacy', ['anzeigen' => 'einheit', 'einheit_id' => $entity->EINHEIT_ID, 'mietvertrag_id' => $entity->MIETVERTRAG_ID]) }}">MV-{{ $entity->MIETVERTRAG_ID }}</a>
+    <a href="{{ route('web::uebersicht::legacy', ['anzeigen' => 'einheit', 'einheit_id' => $entity->einheit->EINHEIT_ID, 'mietvertrag_id' => $entity->MIETVERTRAG_ID]) }}">MV-{{ $entity->MIETVERTRAG_ID }}</a>
+    <a target="_blank" href="{{ route('web::mietkontenblatt::legacy', ['anzeigen' => 'mk_pdf', 'mietvertrag_id' => $entity->MIETVERTRAG_ID]) }}" class="tooltipped" data-position="bottom" data-delay="50" data-tooltip="Mietkontenblatt"><i class="mdi mdi-file-pdf"></i></a>
 </span>
