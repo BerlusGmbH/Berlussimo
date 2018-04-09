121957314e5f04b9a46195e92c84e414593b37f0
Cristian Müller <mueller@berlus.de>
2018-04-09 09:46:34 +0200

    fix: facility managers are now allowed to select partners

--- app/Http/Controllers/Api/v1/Modules/PartnerController.php
+++ app/Http/Controllers/Api/v1/Modules/PartnerController.php
@@ -4,19 +4,20 @@ namespace App\Http\Controllers\Api\v1\Modules;
 
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthenticatedRequest;
 use App\Http\Requests\Legacy\PartnerRequest;
 use App\Models\Partner;
 
 class PartnerController extends Controller
 {
 
-    public function select(PartnerRequest $request, Partner $partner)
+    public function select(AuthenticatedRequest $request, Partner $partner)
     {
         session()->put('partner_id', $partner->PARTNER_ID);
         return ['status' => 'ok'];
     }
 
-    public function unselect(PartnerRequest $request)
+    public function unselect(AuthenticatedRequest $request)
     {
         session()->forget('partner_id');
         return ['status' => 'ok'];
