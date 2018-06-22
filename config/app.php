be4060b2519a4a60ac8d383bfc2d763af7823bec
Cristian Müller <mueller@berlus.de>
2018-06-22 14:50:16 +0200

    change: added package for trusted proxy handeling

--- config/app.php
+++ config/app.php
@@ -178,6 +178,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Fideloper\Proxy\TrustedProxyServiceProvider::class,
 
         /*
          * Application Service Providers...
